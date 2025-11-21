986bc97f17597292922399de3c5a33c7e2157801
diogodion1010-code <diogodion1010@gmail.com>
2025-11-21 17:15:54 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,18 @@
--- main.lua
-print("[main.lua] Carregado!")
+-- Script em ServerScriptService
 
-pcall(function()
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "M-TODO-RUAN",
-        Text = "Script iniciado sem KEY!",
-        Duration = 5
-    })
-end)
+local webhookUrl = "https://discord.com/api/webhooks/1441521246796910624/eULhB5XJbqIfypDogaWuzWuyMJ9f3Ay9cMeZh31uTDQPin1tYqgiPCBHaGakfXG8ymOv"
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local HttpService = game:GetService("HttpService")
 
-local player = game.Players.LocalPlayer
-if player and player.Character then
-    for _, part in pairs(player.Character:GetChildren()) do
-        if part:IsA("BasePart") then
-            part.BrickColor = BrickColor.Random()
-        end
+local remote = ReplicatedStorage:WaitForChild("EnviarWebhook")
+
+remote.OnServerEvent:Connect(function(player, link)
+    -- Previne spam: salva se já enviou
+    if not player:GetAttribute("WebEnviado") then
+        player:SetAttribute("WebEnviado", true)
+        local data = {
+            content = string.format("Link enviado por %s: %s", player.Name, link)
+        }
+        HttpService:PostAsync(webhookUrl, HttpService:JSONEncode(data), Enum.HttpContentType.ApplicationJson)
     end
-end
+end)
